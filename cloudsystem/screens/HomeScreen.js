b0021d6d11eec55a54c4fdde91ad3f7918eac3bc
Nawaf <nawafhunter@gmail.com>
2018-08-10 01:22:31 +0300

    test for me

--- cloudsystem/screens/HomeScreen.js
+++ cloudsystem/screens/HomeScreen.js
@@ -42,7 +42,7 @@ export default class HomeScreen extends React.Component {
             </View>
 
             <Text style={styles.getStartedText}>
-              abdulrahman
+              abdulrahman and nawaf and jehan 
             </Text>
           </View>
 
