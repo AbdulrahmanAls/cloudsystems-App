2a8888d4a4d767703b75c8898e6293bbeb032d0a
AbdulrahmanAls <aa7ttss@gmail.com>
2018-08-10 20:02:56 +0300

    Revert "edited by:Jehan "
    This reverts commit d9e448aeaa926703d83fd857cad74340fa7f00ba.

--- cloudsystem/screens/Login.js
+++ cloudsystem/screens/Login.js
@@ -16,48 +16,10 @@ import { MonoText } from '../components/StyledText';
 export default class Login extends React.Component {
   static navigationOptions = {
     header: null,
-   
   };
 
   render() {
     return (
-      
-      //Jehan's code 
-      <KeyboardAvoidingView style={styles.containerView} behavior="padding">
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      
-       <View style={styles.loginScreenContainer}>
-
-   <Image
-    style={{
-    width: 115,
-    height: 104,
-   marginLeft: 130,
-  marginTop: 150,
-
-  }}
-  source={require('./download.png')}
-/>
-          <View style={styles.loginFormView}>
-          <Text style={styles.logoText}>Cloudsystems</Text>
-          //here we have to put a bar the has the choice of either sing in or sing up, i did the sign in one, edit it the way you like 
-
-            <TextInput placeholder="Username" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} />
-            <TextInput placeholder="Password" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} secureTextEntry={true}/>
-            <Button
-              but              
-              tonStyle={styles.loginButton}
-              onPress={() => this.onLoginPress()}
-              title="Sign in"
-              //there's supposed to be a a link for forgot the password, please add it 
-            />
-          </View>
-        </View>
-      </TouchableWithoutFeedback>
-      </KeyboardAvoidingView>
-    );
-  }// here jehan's code end 
-  
       <Container>
   <Header />
   <Content>
